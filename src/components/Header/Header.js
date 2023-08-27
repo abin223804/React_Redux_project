95bc33b1cd9106e3e6a526ce9f51e0927ddb02a4
Abin s chandran <abinschandran@gmail.com>
2023-08-27 12:11:34 +0530

    just updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
    <header>
     <div className='container'>
-<div>Abin</div>
+<div>ABIN</div>
 
 
 <svg fill="#fff" width="24" height="24" viewBox="0 0 24 24"  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6" fill="white"></path></svg>
